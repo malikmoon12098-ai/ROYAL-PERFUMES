4e14ae71f4aafab8dfbd838e5a0dcf5cf25c2273
malikmoon12098-ai <malikmoon12098@gmail.com>
2026-04-14 12:22:14 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
-// Firebase Removed
-const db = {};
-const collection = () => {};
-const onSnapshot = () => {};
-const query = () => {};
-const orderBy = () => {};
-const doc = () => {};
-const getDoc = async () => ({ exists: () => false });
-const setDoc = async () => {};
-const updateDoc = async () => {};
-const increment = () => 0;
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getFirestore, collection, doc, onSnapshot, getDoc, getDocs, setDoc, addDoc, updateDoc, deleteDoc, query, orderBy, where, increment } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+import { getAuth, signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyvsUME6_x3OW0x8z6RHaxfN5k9uUaYAk",
+  authDomain: "royal-perfumes-5e318.firebaseapp.com",
+  projectId: "royal-perfumes-5e318",
+  storageBucket: "royal-perfumes-5e318.firebasestorage.app",
+  messagingSenderId: "642409364088",
+  appId: "1:642409364088:web:5c712c7807f660af5e0b8d"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+window.db = db;
+window.auth = auth;
+window.provider = provider;
+window.collection = collection;
+window.doc = doc;
+window.onSnapshot = onSnapshot;
+window.getDoc = getDoc;
+window.getDocs = getDocs;
+window.setDoc = setDoc;
+window.addDoc = addDoc;
+window.updateDoc = updateDoc;
+window.deleteDoc = deleteDoc;
+window.query = query;
+window.orderBy = orderBy;
+window.where = where;
+window.increment = increment;
+window.signInWithPopup = signInWithPopup;
+window.signOut = signOut;
+window.onAuthStateChanged = onAuthStateChanged;
 
 
 document.addEventListener('DOMContentLoaded', () => {
